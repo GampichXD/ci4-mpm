34447bdef3a88a85d7082f846d3879b34eb23e57
GampichXD <abrahambram295@gmail.com>
2025-05-06 20:23:00 +0700

    MVC Edit Data

--- app/Views/post-create.php
+++ app/Views/post-create.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <title>Tambah Data - santriKoding.com</title>
+    <title>Tambah Data</title>
 </head>
 
 <body>
